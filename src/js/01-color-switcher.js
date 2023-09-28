4f6d34d14f2ebcf2c8a4f3422bd0046a6c3d12e8
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2023-09-28 17:14:40 +0300

    complete first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  disco: null,
+};
 
+refs.stopBtn.disabled = true;
+// =======================================================================
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+const bodyDisco = () => {
+  refs.body.style.backgroundColor = getRandomHexColor();
+};
+
+
+const startDisco = () => {
+  refs.disco = setInterval(() => bodyDisco(), 1000);
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+};
+
+const stopDisco = () => {
+  clearInterval(refs.disco);
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+};
+
+
+// =========================================================================
+
+refs.startBtn.addEventListener('click', startDisco);
+refs.stopBtn.addEventListener('click', stopDisco);
