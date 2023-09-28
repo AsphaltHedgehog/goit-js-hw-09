b3c4d078f7d04aafc34e41682b43fd471db28c43
AsphaltHedgehog <139024405+AsphaltHedgehog@users.noreply.github.com>
2023-09-28 14:15:05 +0000

    Deploying to gh-pages from @ AsphaltHedgehog/goit-js-hw-09@4f6d34d14f2ebcf2c8a4f3422bd0046a6c3d12e8 🚀

---
+++ 01-color-switcher.0fd3b81b.js
@@ -0,0 +1,2 @@
+const t={body:document.querySelector("body"),startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),disco:null};t.stopBtn.disabled=!0;const e=()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`};t.startBtn.addEventListener("click",(()=>{t.disco=setInterval((()=>e()),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(()=>{clearInterval(t.disco),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.0fd3b81b.js.map
