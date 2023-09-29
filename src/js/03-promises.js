3483f266b40d3835661a46e96eb6bbec6455ffd1
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2023-09-29 20:55:52 +0300

    complete task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,63 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// =========================================================
+
+const refs = {
+  form: document.querySelector('.form'),
+  submitBtn: document.querySelector("button"),
+  startDelay: document.querySelector('[name="delay"]'),
+  stepDelay: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+};
+
+
+// =========================================================
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      };
+    }, delay);
+  });
+};
+
+
+
+// ===========================================================
+
+
+refs.submitBtn.addEventListener('click', function (ev) {
+  ev.preventDefault();
+  // ============================================================
+  const amount = Number(refs.amount.value);
+  const startDelay = Number(refs.startDelay.value);
+  const stepDelay = Number(refs.stepDelay.value);
+
+  // ============================================================
+
+  for (let i = 0; i < amount; i += 1) {
+    let del = startDelay + (stepDelay * i)
+
+    // ===================================================================
+
+    createPromise(i, del)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+  };
+  });
+
+
+
+
+
+
+
